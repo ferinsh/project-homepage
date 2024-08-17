a182e0337b7ecc95af730f2afc20b21c6068f376
Ferin Sharaf <146843308+ferinsh@users.noreply.github.com>
2024-08-18 01:23:36 +0530

    Add new project

--- src/index.js
+++ src/index.js
@@ -8,24 +8,26 @@ document.addEventListener("DOMContentLoaded", ()=> {
     // console.log("DOMContentLoaded");
     let arrProjectLinks = [
         "https://ferinsh.github.io/project-weather-app/",
-        "https://youtube.com",
-        "https://google.com",
-        "https://github.com/ferinsh",
-        "https://instagram.com",
+        // "https://youtube.com",
+        // "https://google.com",
+        // "https://github.com/ferinsh",
+        // "https://instagram.com",
         "https://ferinsh.github.io/tictactoe/",
         "https://ferinsh.github.io/project-game-rps/",
-        "https://https://ferinsh.github.io/project-todo-list/",
+        "https://ferinsh.github.io/project-todo-list/",
+        "https://legal-chatbot-mdow.onrender.com/"
     ];
-    let arrProjectNames = ["Weather App", "Youtube", "Google", "Github", "Instagram", "Tic Tac Toe", "Rock Paper Scissors", "ToDo List"];
+    let arrProjectNames = ["Weather App", "Tic Tac Toe", "Rock Paper Scissors", "ToDo List", "ConstiBot"];
     let arrProjImgSrc = [
         "./images/weatherapp_logo.png",
-        "./images/youtube_logo.png",
-        "./images/google_logo.png",
-        "./images/github_logo.png",
-        "./images/instagram_logo.png",
+        // "./images/youtube_logo.png",
+        // "./images/google_logo.png",
+        // "./images/github_logo.png",
+        // "./images/instagram_logo.png",
         "./images/tictactoe_logo.png",
         "./images/rockpaperscissors_logo.png",
         "./images/todolist_logo.png",
+        "./images/law-book.png"
     ];
     let arrProject = {
         links : arrProjectLinks,
